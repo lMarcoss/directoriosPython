339cea88348c751bb480f441ebeeea667dfcf4fc
Leonardo Marcos Santiago <markos_193@hotmail.com>
2020-04-27 19:06:26 -0500

    Update last_time_edited_file.py
    edit format date to compare

--- last_time_edited_file.py
+++ last_time_edited_file.py
@@ -13,13 +13,14 @@ def find_files(directory, pattern):
                 yield filename
 
 
-datetime_str = '01/01/2020 1:00:00'
-datetime_object = datetime.strptime(datetime_str, '%m/%d/%Y %H:%M:%S')
+datetime_str = '2020-03-01 00:00:00'
+datetime_object = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
 	
 for filename in find_files(os.getcwd(), '*'):
 	last_time = datetime.fromtimestamp(os.path.getmtime(filename)).strftime('%Y-%m-%d %H:%M:%S')
 	last_time_object = datetime.strptime(last_time, '%Y-%m-%d %H:%M:%S')
 	if(last_time_object > datetime_object):
+		print(last_time,last_time_object, '->', datetime_str,datetime_object)
 		print(last_time, '->', filename)
 
 	
